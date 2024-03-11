d19ceadadab602eed94bebcf3ec53fd96dd3108a
DanielRasho <danirayoroldan@gmail.com>
2024-03-10 21:56:52 -0600

    Feat: First draft of routes

---
+++ backend/src/db.js
@@ -0,0 +1,14 @@
+const { Pool } = require('pg')
+
+const pool = new Pool({
+  host: 'localhost:5433',
+  user: 'admin',
+  password: '1234',
+  port: 5433,
+  database: 'blogs_db',
+  max: 10,
+  idleTimeoutMillis: 30000,
+  connectionTimeoutMillis: 1000,
+})
+
+module.exports = pool
