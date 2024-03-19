eb07fe9fab2473817501a3d91310aefc2a9f66c3
DanielRasho <danirayoroldan@gmail.com>
2024-03-19 17:52:59 -0600

    Fix: post object model field names

--- backend/src/models/post.js
+++ backend/src/models/post.js
@@ -10,14 +10,14 @@ class Post {
    * @param {string} thumbnailPath server file path to post's thumbnail
    * @param {string} contentPath server file path to post's content
    */
-  constructor(id, author, title, tags, publishDate, thumbnailPath, contentPath) {
+  constructor(id, author, title, tags, publishDate, thumbnail, content) {
     this.id = id
     this.title = title
     this.author = author
     this.tags = tags
     this.publishDate = publishDate
-    this.thumbnailPath = thumbnailPath
-    this.contentPath = contentPath
+    this.thumbnail = thumbnail
+    this.content = content
   }
 }
 
