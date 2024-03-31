aa8d126154c2ffac142e4a1fc2bcc56261dc2ba1
DanielRasho <danirayoroldan@gmail.com>
2024-03-30 23:24:01 -0600

    Feat: Add post card grid container

--- frontend/src/views/Home.jsx
+++ frontend/src/views/Home.jsx
@@ -11,19 +11,43 @@ function Home() {
       onClick: () => navigate(VIEW_ROUTES.HOME),
     },
   ];
-
+  
   let postsInfo = [
     {
       date: "mar 13,2024",
       title: "hello morning",
       tags: ["allow", "artist"],
-      thumbnail: "https://m.media-amazon.com/images/I/61HP2OWLvkL.jpg",
+      thumbnail: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQDz_gGDhjWQCGsN3qAqPbUWnOYGhye-aaq5g&usqp=CAU",
+    },
+    {
+      date: "mar 13,2024",
+      title: "hhhhhhhhhhhhhhhhhhhhhhhhhhhhhhh",
+      tags: ["allow", "artist"],
+      thumbnail: "https://c.files.bbci.co.uk/F382/production/_123883326_852a3a31-69d7-4849-81c7-8087bf630251.jpg"
     },
     {
       date: "mar 13,2024",
       title: "hello morning",
       tags: ["allow", "artist"],
-      thumbnail: "https://i.ytimg.com/vi/HaK5r_ejdNA/maxresdefault.jpg"
+      thumbnail: "https://phantom-marca.unidadeditorial.es/c9ede2e9ff24c60e07218519c3e67fe6/resize/828/f/jpg/assets/multimedia/imagenes/2022/09/16/16633380346741.jpg",
+    },
+    {
+      date: "mar 13,2024",
+      title: "hello morning",
+      tags: ["allow", "artist"],
+      thumbnail: "https://i.ytimg.com/vi/hak5r_ejdna/maxresdefault.jpg"
+    },
+    {
+      date: "mar 13,2024",
+      title: "hello morning",
+      tags: ["allow", "artist"],
+      thumbnail: "https://m.media-amazon.com/images/i/61hp2owlvkl.jpg",
+    },
+    {
+      date: "mar 13,2024",
+      title: "hello morning",
+      tags: ["allow", "artist"],
+      thumbnail: "https://phantom-marca.unidadeditorial.es/c9ede2e9ff24c60e07218519c3e67fe6/resize/828/f/jpg/assets/multimedia/imagenes/2022/09/16/16633380346741.jpg"
     },
   ];
 
@@ -41,7 +65,15 @@ function Home() {
   return (
     <>
       <TopBar navLinks={navLinks}/>
-      {posts}
+      <div class="home-header font-display">
+        <h1>Blog spotlight</h1>
+        <div class="gradient-container">
+          <img src="./media/gradient-noise-green-red.png" alt="" />
+        </div>
+      </div>
+      <div class="post-container">
+        {posts}
+      </div>
       <Footer></Footer>
     </>
   );
