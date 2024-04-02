2f5788fb8981836f0318be32d6d44c0525edcd19
DanielRasho <danirayoroldan@gmail.com>
2024-04-02 14:27:05 -0600

    Update SQL schema for tags, now accepting whatever lenght

--- backend/schema.sql
+++ backend/schema.sql
@@ -8,7 +8,7 @@ create table post (
 	id serial primary key,
 	author VARCHAR(30) not null,
 	title VARCHAR(45) not null,
-	tags VARCHAR(50),
+	tags text,
 	publish_date timestamp default current_timestamp not null,
 	thumbnail_path text not null,
 	content_path text not null,
