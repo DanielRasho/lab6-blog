9deeeeef43ab972d4fcdb5d448112fd739acef7c
DanielRasho <danirayoroldan@gmail.com>
2024-04-01 21:00:10 -0600

    Feat: Add Tag input to BlogEditor. And connecting input values to component State

--- frontend/src/views/BlogEditor.jsx
+++ frontend/src/views/BlogEditor.jsx
@@ -1,8 +1,11 @@
-function BlogEditor({ mode = "create", blogInfo = {} }) {
-  const [titlePlaceholder, setTitlePlaceholder] = React.useState("");
-
+function BlogEditor() {
+  const [title, setTitle] = React.useState("");
   const [thumbnail, setThumbnail] = React.useState({});
   const [thumbnailURL, setThumbnailURL] = React.useState("");
+  const [tagInput, setTagInput] = React.useState("");
+  const [tags, setTags] = React.useState([]);
+  const [content, setContent] = React.useState({});
+  const MAX_TAGS = 3;
 
   const titleRef = React.useRef();
   const editorRef = React.useRef(null);
@@ -12,20 +15,61 @@ function BlogEditor({ mode = "create", blogInfo = {} }) {
     titleRef.current.style.height = titleRef.current.scrollHeight + "px";
   };
 
+  const handleTitleInput = (event) => {
+    setTitle(event.target.value);
+  };
+
+  const handleTagInput = (event) => {
+    setTagInput(event.target.value);
+  };
+
+  const handleCreateTag = (event) => {
+    if (
+      event.key == "Enter" &&
+      !tags.includes(tagInput) &&
+      tags.length < MAX_TAGS
+    ) {
+      setTags([...tags, tagInput]);
+      setTagInput("");
+    }
+  };
+
+  const handleDeleteTag = (id) => {
+    console.log(id);
+    setTags(tags.filter((tag) => tag !== id));
+  };
+
   const handleFileChange = (e) => {
     if (e.target.files) {
       const thumbnailImage = e.target.files[0];
       setThumbnail(thumbnailImage);
       setThumbnailURL(URL.createObjectURL(thumbnailImage));
+    }
+  };
 
-      /* Convert the selected image to Base64
-      const reader = new FileReader();
-      reader.readAsDataURL(thumbnailImage);
+  const publish = (title, tags, image64, content) => {
+    fetch();
+  };
+
+  const handlePublishPost = async () => {
+    let content = await editorRef.current.save();
+    const reader = new FileReader();
+    let  thumbnailBase64 = reader.result;
+    const fileLoadedPromise = new Promise((resolve, reject) => {
       reader.onload = () => {
-        const base64Data = reader.result;
-        console.log(base64Data);
+        thumbnailBase64 = reader.result;
+        resolve(thumbnailBase64); // Resolve the promise once the file is loaded
       };
-      */
+      reader.onerror = reject; // Reject the promise in case of an error
+    });
+
+    await reader.readAsDataURL(thumbnail);
+
+    try {
+      await fileLoadedPromise; // Wait until the file is loaded
+      console.log(title, tags, thumbnailBase64, content);
+    } catch (error) {
+      console.error("Error loading file:", error);
     }
   };
 
@@ -34,6 +78,7 @@ function BlogEditor({ mode = "create", blogInfo = {} }) {
     const editorInstance = new EditorJS({
       holder: "text-editor",
       placeholder: 'Write something, type "/" for input a command',
+      minHeight: 30,
       tools: {
         header: Header,
         list: List,
@@ -51,16 +96,6 @@ function BlogEditor({ mode = "create", blogInfo = {} }) {
 
     editorRef.current = editorInstance;
 
-    // SET TITLE PLACEHOLDER
-    switch (mode) {
-      case "editor":
-        setTitlePlaceholder("Edit Blog");
-        break;
-      case "create":
-        setTitlePlaceholder("Title here...");
-        break;
-    }
-
     // ADD EVENT WHEN RESIZING
     window.addEventListener("resize", auto_height);
     return () => {
@@ -75,12 +110,36 @@ function BlogEditor({ mode = "create", blogInfo = {} }) {
       <div class="metadata-editor">
         <textarea
           ref={titleRef}
+          value={title}
           onInput={auto_height}
+          onChange={handleTitleInput}
           class="title"
           type="textarea"
-          placeholder={titlePlaceholder}
+          placeholder="Title here..."
           maxLength={45}
         />
+        <div class="tag-container">
+          <input
+            type="text"
+            value={tagInput}
+            onChange={handleTagInput}
+            onKeyDown={handleCreateTag}
+            placeholder="Input a tag..."
+          />
+          <div class="tags-list">
+            {tags.map((tag) => {
+              return (
+                <span key={tag} class="tag">
+                  {tag}
+                  <i
+                    onClick={() => handleDeleteTag(tag)}
+                    class="fa-solid fa-xmark"
+                  ></i>
+                </span>
+              );
+            })}
+          </div>
+        </div>
         <div class="thumbnail-upload-wrapper">
           <img src={thumbnailURL} alt="" />
           <label htmlFor="thumbnail-upload">
@@ -102,7 +161,7 @@ function BlogEditor({ mode = "create", blogInfo = {} }) {
           <span>Delete Draft</span>
           <i class="fa-solid fa-trash-can"></i>
         </button>
-        <button>
+        <button onClick={handlePublishPost}>
           <span>Post</span>
           <i class="fa-solid fa-paper-plane"></i>
         </button>
@@ -110,3 +169,5 @@ function BlogEditor({ mode = "create", blogInfo = {} }) {
     </div>
   );
 }
+
+function createEditor(readonly) {}
